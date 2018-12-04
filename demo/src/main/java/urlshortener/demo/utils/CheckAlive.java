e525fdf59715455d1d1f599d71c5fe6dc36fb355
Alberto <697589@unizar.es>
2018-12-04 18:43:47 +0100

    Comprobar disponibilidad de URIs periódicamente

--- demo/src/main/java/urlshortener/demo/utils/CheckAlive.java
+++ demo/src/main/java/urlshortener/demo/utils/CheckAlive.java
@@ -5,11 +5,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class CheckAlive {
-
-    //@org.springframework.beans.factory.annotation.Autowired
-    public CheckAlive() {
-    }
-
     public int makeRequest(String s) throws IOException {
         URL url = new URL(s);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
