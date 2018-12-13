1d90981ab2660b058d4a049f527f73ac0653966a
lAngel <angelcanal97@gmail.com>
2018-12-13 08:27:56 +0100

    Add URICreate tests

---
+++ demo/src/test/java/urlshortener/demo/domain/URICreateTest.java
@@ -0,0 +1,46 @@
+package urlshortener.demo.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class URICreateTest {
+
+    @Test
+    public void testURICreateUri(){
+        URICreate base = new URICreate().uri("abc");
+
+        assertEquals("abc", base.getUri());
+        base.setUri("abcd");
+        assertEquals("abcd", base.getUri());
+    }
+
+    @Test
+    public void testURICreateEquals(){
+        URICreate base = new URICreate().uri("abc");
+        URICreate base2 = new URICreate().uri("abc");
+        URICreate base3 = new URICreate().uri("abcd");
+
+        assertEquals(base, base);
+        assertNotEquals(base, null);
+        assertNotEquals(base, "abc");
+        assertEquals(base, base2);
+        assertNotEquals(base, base3);
+    }
+
+    @Test
+    public void testURICreateCheckHashcode(){
+        URICreate base = new URICreate().uri("abc");
+
+        assertEquals(96385, base.hashCode());
+    }
+
+    @Test
+    public void testURICreateCheckToString(){
+        URICreate base = new URICreate().uri("abc");
+
+        assertEquals("class URICreate {\n    uri: abc\n}", base.toString());
+    }
+
+}
