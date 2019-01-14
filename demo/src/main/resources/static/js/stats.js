98ce0030d4fd59cdeacadf399dd7df1348bbfa66
Roberto CCU <ccucraft@gmail.com>
2019-01-14 23:32:52 +0100

    Minor fix

--- demo/src/main/resources/static/js/stats.js
+++ demo/src/main/resources/static/js/stats.js
@@ -48,7 +48,4 @@ $(function () {
   $("form").on('submit', function (e) {
     e.preventDefault();
   });
-  $( "#connect" ).click(function() { connect(); });
-  $( "#disconnect" ).click(function() { disconnect(); });
-  $( "#send" ).click(function() { sendName(); });
 });
